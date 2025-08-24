7be0c24f9437cef312c30731c565913193ad93dc
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2025-08-24 12:37:07 +0530

    Update instr_memory.v

--- Modules/instr_memory.v
+++ Modules/instr_memory.v
@@ -1,16 +1,13 @@
 `timescale 1ns / 1ps
 
 module instr_memory(
-input [7:0] addr,       // supports up to 256 words
-output [31:0] instr
-    );
-
-reg [31:0] memory [0:255];  // Memory array: 256 x 32-bit words, 8-bit address -> 256 instructions
-
-assign instr = memory[addr];
-
-initial begin
-    $readmemh("program.mem", memory);  // Load hex values into 'memory'
-end
-
+  input  [31:0] addr,
+  output [31:0] instr
+);
+  reg [31:0] memory [0:255];
+  wire [7:0] word_index = addr[9:2];
+  assign instr = memory[word_index];
+  initial begin
+    $readmemh("program.mem", memory);
+  end
 endmodule
