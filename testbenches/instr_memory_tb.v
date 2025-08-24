f1da6c16dfc699aab23b23baff12db1742f01ba7
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2025-08-24 12:42:48 +0530

    Update instr_memory_tb.v

--- testbenches/instr_memory_tb.v
+++ testbenches/instr_memory_tb.v
@@ -1,31 +1,28 @@
 `timescale 1ns / 1ps
 
 module instr_memory_tb;
-reg [7:0] addr;
+reg [31:0] addr;
 wire [31:0] instr;
 
 instr_memory DUT (
     .addr(addr),
     .instr(instr)   
     );
-    
+
 initial begin
-$display("Time\tAddress\t\tInstruction");
-$monitor("%0t\t%h\t\t%h", $time, addr, instr);
+$display("Time\tAddress\tInstruction");
+$monitor("%0t\t%h\t%h", $time, addr, instr);
 
-addr = 8'h00; 
-#10;
-addr = 8'h01; 
-#10;
-addr = 8'h02; 
-#10;
-addr = 8'h03; 
-#10;
-addr = 8'h04; 
-#10;
-addr = 8'h05; 
-#10;
-
+addr = 32'h00000000; #10;
+addr = 32'h00000004; #10;
+addr = 32'h00000008; #10;
+addr = 32'h0000000C; #10;
+addr = 32'h00000010; #10;
+addr = 32'h00000014; #10;
+addr = 32'h00000018; #10;
+addr = 32'h0000001C; #10;
+addr = 32'h00000020; #10;
+$finish;
 end
     
 endmodule
